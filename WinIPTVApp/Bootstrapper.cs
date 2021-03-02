dcbdeb6c1ac32b71555c5ea703a9711cef0f187f
gr8.yjinhyuk@gmail.com <gr8.yjinhyuk@gmail.com>
2021-03-02 09:53:56 +0800

    Initialize the Basic Window Interface and Set the Styles of App.

--- WinIPTVApp/Bootstrapper.cs
+++ WinIPTVApp/Bootstrapper.cs
@@ -1,13 +1,34 @@
-﻿using Caliburn.Micro.Autofac;
+﻿using Autofac;
+using Caliburn.Micro.Autofac;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace WinIPTVApp
 {
     public class Bootstrapper : AutofacBootstrapper<ShellViewModel>
     {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void ConfigureBootstrapper()
+        {
+            base.ConfigureBootstrapper();
+        }
+
+        protected override void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterType<ShellViewModel>().SingleInstance();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
     }
 }
