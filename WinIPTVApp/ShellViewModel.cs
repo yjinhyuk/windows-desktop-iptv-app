fd7950fba1fbf06d07f20425ef054db983482c85
gr8.yjinhyuk@gmail.com <gr8.yjinhyuk@gmail.com>
2021-03-02 09:14:34 +0800

    Architect the MVVM Design Pattern

---
+++ WinIPTVApp/ShellViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinIPTVApp
+{
+    public class ShellViewModel:Conductor<Screen>.Collection.OneActive
+    {
+        private readonly IEventAggregator _eventAggregator;
+        
+
+        public ShellViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+
+            _eventAggregator.Subscribe(this);
+            
+        }
+
+        protected override void OnDeactivate(bool close)
+        {
+            base.OnDeactivate(close);
+            _eventAggregator.Unsubscribe(this);
+        }
+    }
+}
